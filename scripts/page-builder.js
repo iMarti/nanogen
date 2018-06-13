b9646cfaf16a3971b3f28537dc36d4a827bbe092
Ivan Martinac <ivan.martinac@gmail.com>
2018-06-13 14:03:05 +0200

    Displaying the time pages were built.

--- scripts/page-builder.js
+++ scripts/page-builder.js
@@ -135,6 +135,6 @@ function build(config) {
     var timeDiff = process.hrtime(startTime);
     var duration = timeDiff[0] * 1000 + timeDiff[1] / 1e6;
     var round = function (d) { return Math.round(d * 10) / 10; };
-    console.log("Succesfully built " + builds.length + " pages in " + round(duration) + " ms, " + round(duration / builds.length) + " ms/page");
+    console.log(new Date().toLocaleString() + " Succesfully built " + builds.length + " pages in " + round(duration) + " ms, " + round(duration / builds.length) + " ms/page");
 }
 exports.build = build;
